311dfd5c2b4746ec33a4db2755f80aed25d98175
Grant Galinger <grant.galinger@rawvoice.com>
2024-06-03 08:53:27 -0400

    progress

--- htdocs/welcome.php
+++ htdocs/welcome.php
@@ -8,10 +8,41 @@ $page->topNavigation();
 
 ?>
 
+<div class="row justify-content-center align-items-center" style="margin-top: 100px;">
+
+    <div class="col col-md-auto">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown button
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col col-sm-auto" style="">
+        <h4 class="mb-0">|</h4>
+    </div>
+
+    <div class="col col-md-auto">
+        <h2 class="mb-0 fw-bold">Grant R. Galinger</h2>
+    </div>
+
+    <div class="col col-md-auto" style="padding-top: 4px; margin-left: 250px;">
+        <label class="switch">
+            <input type="checkbox" class="checkbox">
+            <div class="slider"></div>
+        </label>
+    </div>
+</div>
 
 
-<div class="container-fluid" style="background: aliceblue; margin-top: 300px;">
-    <div class="row">
+<div class="container-fluid" style="background: aliceblue;">
+
+    <div class="row" style="margin-top: 300px;">
         <h4 style="">Hi, I am <span class="font-weight-bold">Grant Galinger</span></h4>
     </div>
 
